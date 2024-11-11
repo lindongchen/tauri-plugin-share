2f42e48628425e2cc184ef4dbf8080ff1db38ce6
DongChen Lin <393227948@qq.com>
2024-11-11 23:30:07 +0800

    add api-iife.js

--- build.rs
+++ build.rs
@@ -2,6 +2,7 @@ const COMMANDS: &[&str] = &["share_file","get_shared_files","get_shared_files_pa
 
 fn main() {
   tauri_plugin::Builder::new(COMMANDS)
+		.global_api_script_path("./api-iife.js")
     .android_path("android")
     .ios_path("ios")
     .build();
--- rollup.config.js
+++ rollup.config.js
@@ -2,10 +2,13 @@ import { readFileSync } from 'fs'
 import { join } from 'path'
 import { cwd } from 'process'
 import typescript from '@rollup/plugin-typescript'
+import { nodeResolve } from '@rollup/plugin-node-resolve'
+import terser from '@rollup/plugin-terser'
 
 const pkg = JSON.parse(readFileSync(join(cwd(), 'package.json'), 'utf8'))
-
-export default {
+const pName = "__TAURI_PLUGIN_SHARE__";
+const sName = "share";
+export default [{
   input: 'guest-js/index.ts',
   output: [
     {
@@ -28,4 +31,17 @@ export default {
     ...Object.keys(pkg.dependencies || {}),
     ...Object.keys(pkg.peerDependencies || {})
   ]
-}
+},{
+	input: './guest-js/index.ts',
+	output: {
+		format: "iife",
+		name: pName,
+		banner: "if ('__TAURI__' in window) {",
+		footer: `Object.defineProperty(window.__TAURI__, '${sName}', { value: ${pName} }) }`,
+		file: "api-iife.js",
+	},
+	plugins: [typescript(), terser(), nodeResolve()],
+	onwarn: (warning) => {
+		throw Object.assign(new Error(), warning);
+	},
+}]
