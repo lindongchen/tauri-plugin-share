271b122a6831f1cdd30fa6587abc96cab0ef4825
zhangcheng870518 <zhangcheng@flomesh.cn>
2024-11-14 14:42:46 +0800

    add android plugins

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -41,4 +41,5 @@ dependencies {
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
     implementation(project(":tauri-android"))
+    implementation("com.google.code.gson:gson:2.8.8")
 }
--- android/src/main/java/SharePlugin.kt
+++ android/src/main/java/SharePlugin.kt
@@ -10,7 +10,11 @@ import app.tauri.annotation.TauriPlugin
 import app.tauri.plugin.JSObject
 import app.tauri.plugin.Plugin
 import app.tauri.plugin.Invoke
-import android.util.Log
+import java.nio.file.Files
+import java.nio.file.Path
+import java.nio.file.Paths
+import com.google.gson.JsonArray
+import com.google.gson.Gson
 
 @InvokeArg
 class ShareOptions {
@@ -19,6 +23,13 @@ class ShareOptions {
     var group: String = ""
 }
 
+@InvokeArg
+class ShareFile {
+    var name: String = ""
+    var size: Long = 0
+    var mime: String = ""
+    var data: ByteArray =  ByteArray(1024)
+}
 @TauriPlugin
 class SharePlugin(private val activity: Activity): Plugin(activity) {
 
@@ -29,6 +40,7 @@ class SharePlugin(private val activity: Activity): Plugin(activity) {
 		val context = activity.applicationContext
 	 
 		val file = File(args.path)
+        println("SharePlugin getSharedFiles: $args.path")
 		val uri = FileProvider.getUriForFile(
 			context,
 			"${context.packageName}.fileprovider",
@@ -39,5 +51,53 @@ class SharePlugin(private val activity: Activity): Plugin(activity) {
 		intent.putExtra(Intent.EXTRA_STREAM, uri);
 		intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
 		context.startActivity(intent)
+	}
+
+	@Command
+	fun getSharedFilesPath(invoke: Invoke) {
+		val args = invoke.parseArgs(ShareOptions::class.java)
+		val context = activity.applicationContext
+		val appDir = context.filesDir
+        val sharedPath = File(appDir, args.path)
+
+		val paths = ArrayList<String>()
+		sharedPath.listFiles()?.forEach { file ->
+        	println("SharePlugin getSharedFilesPath: $file")
+            paths.add(file.absolutePath)
+        }
+		val ret = JSObject()
+		ret.put("paths", paths.joinToString(";"))
+		invoke.resolve(ret)
+        
+	}
+
+	@Command
+	fun getSharedFiles(invoke: Invoke) {
+		val args = invoke.parseArgs(ShareOptions::class.java)
+		val context = activity.applicationContext
+		val appDir = context.filesDir
+        val sharedPath = File(appDir, args.path)
+
+		val files = ArrayList<ShareFile>()
+		sharedPath.listFiles()?.forEach { file ->
+			val path: Path = Paths.get(file.absolutePath)
+			val mimeType = Files.probeContentType(path)
+			val fileData: ByteArray = Files.readAllBytes(path)
+			val fileObj = ShareFile()
+			fileObj.name = file.name
+			fileObj.size = file.length()
+			fileObj.mime = mimeType
+			fileObj.data = fileData
+            files.add(fileObj)
+
+			file.delete()
+        }
+        println("SharePlugin getSharedFiles: $files")
+		val gson = Gson()
+		val jsonArray = gson.toJsonTree(files).asJsonArray
+		val ret = JSObject()
+		ret.put("files", jsonArray)
+		invoke.resolve(ret)
+        
 	}
 }
